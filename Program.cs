cd04de740d0935b73e73897ccb2c462be80f2214
Pollyana <pollyanarocha416@gmail.com>
2022-06-08 20:53:09 -0300

    Tempo de viagem

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Switch_case
+{
+     class Program
+    {
+        static void Main()
+        {
+            int tempo = 0;
+            char escolha;
+            
+            // v1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nA viagem de Belo Horizonte/BH a Vitoria/ES em minutos: ");
+            Console.Write("Escolha o transporte: [a]Avião | [c]Carro | [o]Ônibus : ");
+            escolha = Convert.ToChar(Console.ReadLine());
+        
+            switch (escolha)
+            {
+                case 'a':
+                    tempo = 50;
+                    break;
+                case 'c':
+                    tempo = 480;
+                    break;
+                case 'o':
+                    tempo = 660;
+                    break;
+                default:
+                    tempo = 1;
+                    break;
+
+            }
+
+            if(tempo<0)
+            {
+                Console.WriteLine("Transporte indisponivel");
+            }else
+            {
+                Console.WriteLine("Para o transporte escolhido o tempo é: {0}", tempo);
+            }
+        }
+    }
+}
